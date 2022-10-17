8bbbf2472ee98a659be5556ec625361449c036a5
Michael Groeneweg <75755543+michaelgroeneweg@users.noreply.github.com>
2022-10-17 18:08:53 +0200

    UseDarkmode added with toggle in header.

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
 
   return (
     <div className="flex flex-col gap-4 m-w-[50rem] w-[50rem]">
-        <header className="bg-gray-700 p-6 rounded">test</header>
-        <article className="bg-gray-700 p-6 rounded">
+        <header className="bg-gray-300 dark:bg-gray-700 p-6 rounded">test</header>
+        <article className="bg-gray-300 dark:bg-gray-700 p-6 rounded">
             test
         </article>
     </div>
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,29 @@
-import React, { ReactNode, FC } from 'react'
+import React, {ReactNode, FC, useEffect } from 'react'
+import { MoonIcon } from '@heroicons/react/24/outline'
+import { useDarkMode } from 'usehooks-ts'
 
 type Props = { children: React.ReactNode };
 
 const Layout: React.FC<Props>  =  ({children}) => {
+    const { isDarkMode, toggle } = useDarkMode()
+
+    useEffect(() => {
+        document.body.className = isDarkMode ? 'dark' : '';
+    }, [isDarkMode]);
+
     return (
-        <div className="">
-            <header className="fixed py-6 px-10">test</header>
-            <main className="grid place-items-center h-screen bg-gray-900">
+        <div className="bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-200">
+            <header className="fixed py-6 px-10 w-screen">
+                <div className="flex justify-between p-0">
+                    <div className="flex-1">test</div>
+                    <div className="flex flex-row-reverse">
+                        <button onClick={toggle}>
+                            <MoonIcon className="h-6 w-6 dark:text-gray-100 text-gray-700"/>
+                        </button>
+                    </div>
+                </div>
+            </header>
+            <main className="grid place-items-center h-screen bg-gray-200 dark:bg-gray-900">
                 {children}
             </main>
         </div>
